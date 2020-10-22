5ae2f6330d2ef9b18d5c7b6934d160fece3471f6
Tim Morton <tim.morton@gmail.com>
2020-10-21 23:27:06 -0700

    make lists earlier

--- navigator/gui.py
+++ navigator/gui.py
@@ -23,14 +23,14 @@ def get_plots_list(df, filt, datastyle, number, category, compare=False, full_na
     q = f'filt == "{filt}" and datastyle == "{datastyle}" and number == {number} and category == "{category}" and compare == {compare}'
     
     if full_name:
-        plots = df.query(q).basename
+        plots = list(df.query(q).basename)
     else:
-        plots = df.query(q).content
+        plots = list(df.query(q).content)
 
-    paths = df.query(q).filename
+    paths = list(df.query(q).filename)
 
     inds = np.argsort(plots)
-    plots = list(plots[inds])
-    paths = list(paths[inds])
+    plots = plots[inds]
+    paths = paths[inds]
 
     return plots, paths
