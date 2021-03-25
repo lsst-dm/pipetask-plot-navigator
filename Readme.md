90fd4280a985d2699645519c3025ef408bd55de5
T. Andrew Manning <manninga@illinois.edu>
2021-03-25 09:04:09 -0500

    Add sync_container.sh script and docs about how to use it

--- Readme.md
+++ Readme.md
@@ -22,10 +22,24 @@ docker run --rm --name pipetask-plot-navigator \
     pipetask-plot-navigator:latest \
     panel serve dashboard_gen3.py --port 8080 --dev
 ```
+
+Remote Development
+------------------------
+
+Remote development is tricky, because the code is running in a container on the Kubernetes cluster. To iterate on this remote code during development, you can use the `sync_container.sh` script to copy the local source code to the running container. The full command with the namespace, source directory, and target directory arguments looks like this:
+
+```
+./sync_container.sh lsst-dm-pipetask-plot-navigator src /home/worker/app
+```
+
+Once the files are copied to the container and overwrite them, the container's running command, `panel serve ...`, will automatically restart because it is using the `--dev` option [that watches for changes to certain filetypes](https://panel.holoviz.org/user_guide/Deploy_and_Export.html). Refreshing the webpage will reload the app with the modified code. 
+
+**Note**: If your pod is recreated for any reason, the synced code will be lost because the container image will be loaded. You will need to resync the code changes that are not already pushed to the Docker image repo.
+
 Build
 ------------------------
 
-The Pipe Analysis Navigator container image is built automatically by GitHub Actions when a commit modifying the `/src` folder is pushed to the `main` branch. See `/.github/workflows/build-pipetask-plot-navigator.yaml`. 
+The Pipetask Plot Navigator container image is built automatically by GitHub Actions when a commit modifying the `/src` folder is pushed to the `main` branch. See `/.github/workflows/build-pipetask-plot-navigator.yaml`. 
 
 To reduce build time, the [GitHub Cache](https://docs.github.com/en/actions/guides/caching-dependencies-to-speed-up-workflows) system is used to mimic the way Docker caches image layers when building locally. This technique is still relatively new and [is a temporary hack](https://github.com/docker/build-push-action/blob/master/docs/advanced/cache.md#github-cache).
 
---
+++ sync_container.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+# Default namespace
+DEFAULT_NAMESPACE="lsst-dm-pipetask-plot-navigator"
+# Value of --selector= argument for obtaining target pod
+POD_SELECTOR="app=pipetask-plot-navigator"
+# Source directory relative to this script's path
+DEFAULT_SOURCE_DIR="src"
+# Target directory on remote container
+DEFAULT_TARGET_DIR="/home/worker/app"
+
+# Set the default namespace if none is provided as the first argument
+NAMESPACE="$1"
+if [[ "X${NAMESPACE}" == "X" ]]; then
+  NAMESPACE="${DEFAULT_NAMESPACE}"
+fi
+# Set the default source dir if none is provided as the first argument
+SOURCE_DIR="$2"
+if [[ "X${SOURCE_DIR}" == "X" ]]; then
+  SOURCE_DIR="${DEFAULT_SOURCE_DIR}"
+fi
+# Set the default target dir if none is provided as the first argument
+TARGET_DIR="$3"
+if [[ "X${TARGET_DIR}" == "X" ]]; then
+  TARGET_DIR="${DEFAULT_TARGET_DIR}"
+fi
+
+# Move to working directory containing this script, because paths
+# are assumed to be relative to its location
+scriptPath="$(readlink -f "$0")"
+scriptDir="$(dirname "${scriptPath}")"
+cd "${scriptDir}" || exit 1
+
+# Get the pod name
+POD_ID="$(kubectl get pod -n ${NAMESPACE} --selector=${POD_SELECTOR} -o jsonpath='{.items[0].metadata.name}')"
+
+echo "Updating source code in ${POD_ID}..."
+# Generate a unique string to verify that the transfer was successful
+TIMESTAMP="$(date | shasum | cut -f1 -d ' ')"
+echo "${TIMESTAMP}" > "${SOURCE_DIR}/.codesync"
+# Count the number of forward slashes in the source path so that
+# the tar command can discard those parent directories
+STRIP_NUM="$(echo "${SOURCE_DIR}" | tr '/' ' ' | wc -w)"
+# Copy the files into the remote container
+tar cf - "${SOURCE_DIR}" | \
+    kubectl exec -i -n "${NAMESPACE}" "${POD_ID}" -- \
+    tar --overwrite -xf - --strip-components="${STRIP_NUM}" -C "${TARGET_DIR}"
+# Compare the remote .codesync file contents with the expected value
+LASTSYNC="$(kubectl exec -it -n "${NAMESPACE}" "${POD_ID}" -- cat "${TARGET_DIR}/.codesync" | tr -d '\n' | tr -d '\r')"
+if [[ "${LASTSYNC}" == "${TIMESTAMP}" ]]; then
+  echo "Sync successful."
+  exit 0
+else
+  echo "Sync failed."
+  exit 1
+fi
+
+# Clean up temp files
+rm -f "${SOURCE_DIR}/.codesync"
