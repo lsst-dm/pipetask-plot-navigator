c27e5c71658176189592fcabc4e25157499f1b71
Colin Slater <ctslater@uw.edu>
2021-04-30 14:16:03 -0700

    Use boostrap theme.

--- src/dashboard_gen3.py
+++ src/dashboard_gen3.py
@@ -21,8 +21,6 @@ from pathlib import Path
 import re
 import panel as pn
 
-pn.extension()
-
 import lsst.daf.butler as dafButler
 
 config = None
@@ -38,6 +36,7 @@ collections2 = []
 pattern = re.compile(".*Plot.*")
 plot_paths = {}
 plot_paths2 = {}
+
 
 pn.extension()
 
@@ -295,20 +294,36 @@ def refresh(event):
         
 refresh_button.on_click(refresh)
 
-gspec = pn.GridSpec(sizing_mode="stretch_height", max_height=800)
 
-repo_collection_tabs = pn.Tabs(('Collection 1', pn.Column(repo_select, collection_select)), 
+bootstrap = pn.template.BootstrapTemplate(title="Rubin Plot Navigator")
+
+repo_collection_tabs = pn.Tabs(('Collection 1', pn.Column(repo_select, collection_select)),
                                ('Collection 2', pn.Column(repo2_select, collection2_select)))
 
-gspec[0, 0:2] = root_entry
-gspec[1:3, 0:2] = repo_collection_tabs
-gspec[3, 0:2] = refresh_button
-gspec[4, 0:2] = tract_select
-gspec[5, 0:2] = plot_filter
-gspec[6:12, 0:2] = plot_select
-gspec[0, 2:4] = debug_text
-gspec[1, 2] = width_entry
-gspec[1, 3] = ncols_entry
-gspec[2:, 2:4] = pn.Tabs(('collection 1', plots), ('collection 2', plots2))
 
-gspec.servable()
+bootstrap.sidebar.append(root_entry)
+bootstrap.sidebar.append(repo_collection_tabs)
+bootstrap.sidebar.append(refresh_button)
+bootstrap.sidebar.append(tract_select)
+bootstrap.sidebar.append(plot_filter)
+bootstrap.sidebar.append(plot_select)
+
+bootstrap.sidebar.append(width_entry)
+bootstrap.sidebar.append(ncols_entry)
+
+bootstrap.main.append(debug_text)
+bootstrap.main.append(pn.Tabs(('collection 1', plots), ('collection 2', plots2)))
+
+
+with open("bootstrap_override.css") as f:
+    bootstrap.config.raw_css.append(f.read())
+
+bootstrap.header_background = "#1f2121"
+bootstrap.header_color = "#058b8c"
+bootstrap.header.append(pn.pane.HTML("<div class=\"header_link header_selected\" >Tract-based plots</div>"))
+bootstrap.header.append(pn.pane.HTML("<a class=\"header_link\" href=\"/visit_dashboard\">Visit-based plots</a>"))
+
+
+bootstrap.servable()
+
+
