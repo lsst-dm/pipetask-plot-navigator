644568bfe8efe3be53a525d1e1637b06afe9ac93
Colin Slater <ctslater@uw.edu>
2021-04-30 14:29:54 -0700

    Convert visit dashboard to bootstrap.

--- src/visit_dashboard.py
+++ src/visit_dashboard.py
@@ -18,8 +18,11 @@ butler = dafButler.Butler(config="test_repo/butler.yaml", run="test_run")
 
 def update_image():
     next_visit = "file_{:03d}".format(int(counter_widget.value))
-    uri = butler.getURI("visitPlot_demo", visit=int(counter_widget.value),
-                     instrument="HSC")
+    try:
+        uri = butler.getURI("visitPlot_demo", visit=int(counter_widget.value),
+                         instrument="HSC")
+    except KeyError:
+        return
     plot_pane.objects = [panel.pane.PNG(uri, width=600)]
     displayed_visit = counter_widget.value
 
@@ -37,10 +40,24 @@ def poll_next_image():
 
 panel.state.add_periodic_callback(poll_next_image, period=1000, start=True)
 
-widget_column = panel.Column(counter_widget, plot_pane)
-
 update_image()
-widget_column.servable()
+
+
+bootstrap = panel.template.BootstrapTemplate(title="Rubin Plot Navigator")
+
+bootstrap.sidebar.append(counter_widget)
+bootstrap.main.append(plot_pane)
+
+with open("bootstrap_override.css") as f:
+    bootstrap.config.raw_css.append(f.read())
+
+bootstrap.header_background = "#1f2121"
+bootstrap.header_color = "#058b8c"
+bootstrap.header.append(panel.pane.HTML("<a class=\"header_link\" href=\"/dashboard_gen3\">Tract-based plots</div>"))
+bootstrap.header.append(panel.pane.HTML("<div class=\"header_link header_selected\">Visit-based plots</div>"))
+
+
+bootstrap.servable()
 
 
 
