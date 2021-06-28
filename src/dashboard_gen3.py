c52bfa58888f95da53dee45723c67cd26a9e93b4
Tim Morton <tim.morton@gmail.com>
2021-06-28 01:21:08 -0700

    add skymap widget

--- src/dashboard_gen3.py
+++ src/dashboard_gen3.py
@@ -4,14 +4,14 @@ import logging
 # Configure logging
 log = logging.getLogger(__name__)
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+formatter = logging.Formatter("%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 log.addHandler(handler)
 try:
-    log.setLevel(os.environ['LOG_LEVEL'].upper())
+    log.setLevel(os.environ["LOG_LEVEL"].upper())
 except:
-    log.setLevel('DEBUG')
-    
+    log.setLevel("DEBUG")
+
 from pathlib import Path
 import re
 
@@ -19,24 +19,32 @@ import panel as pn
 import param
 
 from lsst.daf.butler import Butler
+from lsst.skymap import BaseSkyMap
 
 pn.extension()
 
-default_repo = '/project/hsc/gen3repo/rc2w02_ssw03'
+default_repo = "/project/hsc/gen3repo/rc2w02_ssw03"
 
-def find_types(registry, storageClassName='Plot'):
+
+def find_types(registry, storageClassName="Plot"):
     types = []
     for t in registry.queryDatasetTypes():
         try:
             if t.storageClass.name == storageClassName:
                 types.append(t)
         except:
-            pass    
+            pass
 
     return types
-        
+
+
+def find_skymaps(registry, collection):
+    return list(set(x.dataId["skymap"] for x in registry.queryDatasets("skyMap", collections=collection)))
+
+
 def find_refs(registry, collection, types):
     return list(registry.queryDatasets(types, collections=collection, findFirst=True))
+
 
 def get_tracts(refs):
     tracts = list(set(r.dataId["tract"] for r in refs))
@@ -45,61 +53,62 @@ def get_tracts(refs):
 
 
 class PlotExplorer(param.Parameterized):
-        
+
     repo1 = param.String(default=default_repo)
-    load1 = param.Action(lambda x: x.param.trigger('load1'), label='Load Butler 1')
-    collection1 = param.ObjectSelector(default='', objects=[])
+    load1 = param.Action(lambda x: x.param.trigger("load1"), label="Load Butler 1")
+    collection1 = param.ObjectSelector(default="", objects=[])
 
     repo2 = param.String(default=default_repo)
-    load2 = param.Action(lambda x: x.param.trigger('load2'), label='Load Butler 2')
-    collection2 = param.ObjectSelector(default='', objects=[])
-        
+    load2 = param.Action(lambda x: x.param.trigger("load2"), label="Load Butler 2")
+    collection2 = param.ObjectSelector(default="", objects=[])
+
     butler1 = param.ClassSelector(default=None, class_=Butler, precedence=-1)
     butler2 = param.ClassSelector(default=None, class_=Butler, precedence=-1)
-        
+
+    skymap = param.ObjectSelector(objects=[])
     tract = param.ListSelector(objects=[])
     name_filter = param.String()
     plot_select = param.ListSelector(objects=[])
-        
+
     plot_width = param.Integer(600, bounds=(300, 1200))
     ncols = param.Integer(2)
-        
+
     debug = pn.widgets.StaticText(value=f"Status message.")
-        
+
     plot_paths = param.Dict(precedence=-1)
     plot_paths2 = param.Dict(precedence=-1)
-        
+
     def _load_butler(self, repo_path):
-        log.debug(f'Loading butler repo from {repo_path}')
+        log.debug(f"Loading butler repo from {repo_path}")
         try:
             butler = Butler(repo_path)
             return butler
         except Exception as e:
-            self.debug.value = f'Failed to load Butler: {str(e)}'
-            log.error(f'{str(e)}')
-        
-    @param.depends('load1', watch=True)
+            self.debug.value = f"Failed to load Butler: {str(e)}"
+            log.error(f"{str(e)}")
+
+    @param.depends("load1", watch=True)
     def _update_butler1(self):
         self.butler1 = self._load_butler(self.repo1)
         if self.butler1 is not None:
-            self.debug.value = f'Loaded Butler1 from {self.repo1}.'
+            self.debug.value = f"Loaded Butler1 from {self.repo1}."
             collections = list(self.butler1.registry.queryCollections())
             collections.sort()
             self.param.collection1.objects = collections
 
-    @param.depends('load2', watch=True)
+    @param.depends("load2", watch=True)
     def _update_butler2(self):
         self.butler2 = self._load_butler(self.repo2)
         if self.butler2 is not None:
-            self.debug.value = f'Loaded Butler2 from {self.repo2}.'
+            self.debug.value = f"Loaded Butler2 from {self.repo2}."
             collections = list(self.butler2.registry.queryCollections())
             collections.sort()
             self.param.collection2.objects = collections
 
-    @param.depends('collection1', 'collection2', watch=True)
+    @param.depends("collection1", "collection2", watch=True)
     def _update_tract_select(self):
         registry = self.butler1.registry
-        
+
         types = find_types(registry)
 
         if registry is not None:
@@ -109,24 +118,27 @@ class PlotExplorer(param.Parameterized):
                     refs2 = find_refs(registry, self.collection2, types)
                 else:
                     refs2 = refs
-                                
+
                 self.param.tract.objects = list(set(get_tracts(refs)).intersection(set(get_tracts(refs2))))
                 self.tract = []
+
+                self.param.skymap.objects = find_skymaps(registry, self.collection1)
+                self.skymap = self.param.skymap.objects[0]
             except:
-                raise        
-        
-    @param.depends('tract', 'collection1', 'collection2', 'name_filter', watch=True)
+                raise
+
+    @param.depends("tract", "collection1", "collection2", "name_filter", watch=True)
     def _update_plot_names(self):
-        
+
         registry = self.butler1.registry
         registry2 = self.butler2.registry if self.butler2 is not None else None
-        
+
         plot_names = []
         plot_names2 = []
         plot_refs = []
         plot_refs2 = []
         types = find_types(registry)
-        
+
         for tract in self.tract:
             refs = [
                 ref
@@ -134,7 +146,7 @@ class PlotExplorer(param.Parameterized):
                     types,
                     collections=self.collection1,
                     findFirst=True,
-                    dataId={"tract": tract},
+                    dataId={"tract": tract, "skymap": self.skymap},
                 )
                 if re.search(self.name_filter, ref.datasetType.name)
             ]
@@ -146,7 +158,7 @@ class PlotExplorer(param.Parameterized):
                         types,
                         collections=self.collection2,
                         findFirst=True,
-                        dataId={"tract": tract},
+                        dataId={"tract": tract, "skymap": self.skymap},
                     )
                     if re.search(self.name_filter, ref.datasetType.name)
                 ]
@@ -173,49 +185,54 @@ class PlotExplorer(param.Parameterized):
 
         plot_names = list(set(plot_names).intersection(plot_names2))
         plot_names.sort()
-        
-        self.param.plot_select.objects = plot_names     
-        
-    @param.depends('plot_select', 'butler1', 'butler2', 'collection1', 'collection2')
+
+        self.param.plot_select.objects = plot_names
+
+    @param.depends("plot_select", "butler1", "butler2", "collection1", "collection2")
     def plots(self):
         if self.plot_select is not None:
-            plot_list = [pn.pane.PNG(self.plot_paths[name], width=self.plot_width) 
-                         for name in self.plot_select]
+            plot_list = [
+                pn.pane.PNG(self.plot_paths[name], width=self.plot_width) for name in self.plot_select
+            ]
             plot_gbox = pn.GridBox(*plot_list, ncols=self.ncols)
-                            
+
             if self.plot_paths2:
-                plot_list2 = [pn.pane.PNG(self.plot_paths2[name], width=self.plot_width) 
-                             for name in self.plot_select]
+                plot_list2 = [
+                    pn.pane.PNG(self.plot_paths2[name], width=self.plot_width) for name in self.plot_select
+                ]
                 plot_gbox2 = pn.GridBox(*plot_list2, ncols=self.ncols)
             else:
                 plot_gbox2 = pn.GridBox([])
 
-            return pn.Tabs(('collection 1', plot_gbox), ('collection 2', plot_gbox2))
-        
+            return pn.Tabs(("collection 1", plot_gbox), ("collection 2", plot_gbox2))
+
     def panel(self):
         gspec = pn.GridSpec(sizing_mode="stretch_height", max_height=800)
-        
-        repo_collection_tabs = pn.Tabs(('Collection 1', pn.Column(self.param.repo1, self.param.load1, self.param.collection1)), 
-                                       ('Collection 2', pn.Column(self.param.repo2, self.param.load2, self.param.collection2)))
 
-        
+        repo_collection_tabs = pn.Tabs(
+            ("Collection 1", pn.Column(self.param.repo1, self.param.load1, self.param.collection1)),
+            ("Collection 2", pn.Column(self.param.repo2, self.param.load2, self.param.collection2)),
+        )
+
         gspec[0:3, 0:2] = repo_collection_tabs
-        
-        gspec[3, 0:2] = self.param.tract
-        gspec[4, 0:2] = self.param.name_filter
-        gspec[5:12, 0:2] = self.param.plot_select
-        
+
+        gspec[3, 0:2] = self.param.skymap
+        gspec[4, 0:2] = self.param.tract
+        gspec[5, 0:2] = self.param.name_filter
+        gspec[6:12, 0:2] = self.param.plot_select
+
         gspec[0, 2:4] = self.debug
-        gspec[1, 2] = pn.Param(self.param.plot_width, 
-                               widgets={'plot_width': 
-                                        {'widget_type': pn.widgets.IntInput,
-                                        'start': 300,
-                                        'end': 1200,
-                                        'step': 50}})
+        gspec[1, 2] = pn.Param(
+            self.param.plot_width,
+            widgets={
+                "plot_width": {"widget_type": pn.widgets.IntInput, "start": 300, "end": 1200, "step": 50}
+            },
+        )
         gspec[1, 3] = self.param.ncols
         gspec[2:, 2:4] = self.plots
-        
+
         return gspec
-    
+
+
 explorer = PlotExplorer()
 pn.Column(explorer.panel).servable()
